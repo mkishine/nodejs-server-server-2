8cffc07e92f5330aa8e29aa5359b1b5d0bc51eb1
Michael Kishinevsky <mkishine@yahoo.com>
2019-04-29 21:35:02 -0400

    Fixed typo I have created for checking validation logic.

--- service/GreeterService.js
+++ service/GreeterService.js
@@ -11,7 +11,7 @@ exports.getGreeting = function (name) {
     return new Promise(function (resolve) {
         let examples = {};
         examples['application/json'] = {
-            "___greeting": "Greeting, " + name + "!"
+            "greeting": "Greeting, " + name + "!"
         };
         if (Object.keys(examples).length > 0) {
             resolve(examples[Object.keys(examples)[0]]);
